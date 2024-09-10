46d92a50a93d4bcf3d1fe40c043ced92e5b845ab
Tyler <minhphu.pham@g.austincc.edu>
2024-09-10 00:23:42 +0000

    yes

--- test/homework/01_data_types/01_data_types_tests.cpp
+++ test/homework/01_data_types/01_data_types_tests.cpp
@@ -1,7 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "data_types.h"
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification")
+{
 	REQUIRE(true == true);
 }
 
@@ -11,3 +12,8 @@ TEST_CASE("Verify sum_numbers function")
 	REQUIRE(add_numbers(10, 10) == 20);
 }
 
+TEST_CASE("Test_Multiply_Numbers")
+{
+	REQUIRE(multiply_numbers(10) == 50);
+	REQUIRE(multiply_numbers(2) == 10);
+}
