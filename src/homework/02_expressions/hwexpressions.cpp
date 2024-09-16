50e06ad3d45c842eac4f7f8162b9f0a081cf09cc
Tyler <minhphu.pham@g.austincc.edu>
2024-09-16 23:09:09 +0000

    hw2

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -8,14 +8,14 @@ int add_numbers(int num1, int num2)
 
 //write function code here
 
-double get_sales_tax_amount(double meal_amount)
+double get_sales_tax_amount(double meal_amount) //This function returns a double data type.
 {
-	double tax_rate = 6.75;
-	return (tax_rate * meal_amount)/100;
+	double tax_rate = 0.0675; //This function creates a constant local variable.
+	return (tax_rate * meal_amount); //This function calculates and returns tax_amount.
 }
 
-double get_tip_amount(double meal_amount, double tip_rate)
+double get_tip_amount(double meal_amount, double tip_rate) //This function also returns the same data type like the above function.
 {
-	return (meal_amount * tip_rate)/100;
+	return (meal_amount * tip_rate)/100; //This function calculates and returns tip_amount.
 }
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -9,14 +9,15 @@ using std::cout; using std::cin;
 
 int main()
 {
+	//Functions below create double variables.
 	double meal_amount;
 	double tip_rate;
 	double tip_amount;
 	double tax_amount;
 	double total;
 
-	cout<<"Enter the meal amount: $";
-	cin>>meal_amount;
+	cout<<"Enter the meal amount: $"; //This function accepts data from the user.
+	cin>>meal_amount; //This function assigns the received data to the meal_amount variable.
 
 	tax_amount = get_sales_tax_amount(meal_amount);
 
