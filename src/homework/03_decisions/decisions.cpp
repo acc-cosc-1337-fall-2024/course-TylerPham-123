581e60606b4d4e3cb29c81dbd02940a5c2471d44
Tyler <minhphu.pham@g.austincc.edu>
2024-09-20 20:38:32 +0000

    hw_03

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,62 @@
 //write include statement for decisions header
 
+#include "decisions.h"
+#include <iostream>
+
+using namespace std;
 
 //Write code for function(s) code here
+
+char get_letter_grade_using_if (int grade)
+{
+    char letter_grade;
+
+    if (grade >= 90 && grade <= 100)
+    {
+        letter_grade = 'A';
+    }
+    else if (grade >= 80 && grade <= 89)
+    {
+        letter_grade = 'B';
+    }
+    else if (grade >= 70 && grade <= 79)
+    {
+        letter_grade = 'C';
+    }
+    else if (grade >= 60 && grade <= 69)
+    {
+        letter_grade = 'D';
+    }
+    else if (grade >= 00 && grade <= 59)
+    {
+        letter_grade = 'F';
+    }
+
+    return letter_grade;
+}
+
+char get_letter_grade_using_switch (int grade)
+{
+    char letter_grade;
+
+    switch (grade)
+    {
+        case 90 ... 100:
+            letter_grade = 'A';
+            break;
+        case 80 ... 89:
+            letter_grade = 'B';
+            break;
+        case 70 ... 79:
+            letter_grade = 'C';
+            break;
+        case 60 ... 69:
+            letter_grade = 'D';
+            break;
+        case 0 ... 59:
+            letter_grade = 'F';
+            break;
+    }
+
+    return letter_grade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,43 @@
 //write include statements
+#include "decisions.h"
+#include <iostream>
+
+using std::cout; using std::cin;
 
 int main() 
 {
+	int option;
+	int grade;
+	char letter_grade;
+
+	cout << "MAIN MENU\n";
+	cout << "1 - Letter grade using if\n";
+	cout << "2 - Letter grade using switch\n";
+	cout << "3 - Exit\n";
+
+	cout << "What is your option? ";
+	cin >> option;
+
+	switch (option)
+	{
+		case 1:
+			cout << "What is your grade? ";
+			cin >> grade;
+
+			letter_grade = get_letter_grade_using_if(grade);
+			cout << "Your letter grade is " << letter_grade << ".\n";
+			break;
+		case 2:
+			cout << "What is your grade? ";
+			cin >> grade;
+
+			letter_grade = get_letter_grade_using_switch(grade);
+			cout << "Your letter grade is " << letter_grade << ".\n";
+			break;
+		case 3:
+			break;
+		default:
+			cout << "The number is out of range.\n";
+	}
 	return 0;
 }
