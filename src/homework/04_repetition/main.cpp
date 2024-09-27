f26d211642aa5204ef048dd1a40874011f8c3f02
Tyler <minhphu.pham@g.austincc.edu>
2024-09-27 23:22:38 +0000

    hw_4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,10 @@
 //write include statements
+#include "repetition.h"
 
+using std::cin; using std::cout;
 //write using statements
 
 int main() 
-{
+{	
 	return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,38 @@
 //add include statements
+#include "repetition.h"
 
 //add function(s) code here
+int factorial(int num)
+{
+    auto product = 1;
+
+    do
+    {   
+        product *= num;
+        num--;   
+    }while(num > 0);
+
+    return product;
+}
+int gcd(int num1, int num2)
+{  
+   auto temp = 0;
+
+   do
+   {
+    if(num1 < num2)
+    {
+        temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        num1 -= num2;
+    }
+    else
+    {
+        num1-=num2;
+    }
+   }while(num1 != num2);
+
+   return num1;
+}
