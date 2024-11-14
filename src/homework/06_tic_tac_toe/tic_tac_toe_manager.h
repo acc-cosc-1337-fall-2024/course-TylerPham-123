5053662d0e9bfe88abae0cff23e92788ec03a989
Tyler <minhphu.pham@g.austincc.edu>
2024-11-13 18:01:12 -0600

    homework 8

--- src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,23 @@
 //h
+#include "tic_tac_toe.h"
+#ifndef TIC_TAC_TOE_MANAGER_H 
+#define TIC_TAC_TOE_MANAGER_H
+
+class TicTacToeManager
+{
+public:
+    void save_game(TicTacToe b);
+    void get_winner_total(int& o, int& w, int& t) {o = x_win; w = o_win; t = ties;};
+
+private:
+    std::vector <TicTacToe> games;
+
+//Private members
+    int x_win = 0;
+    int o_win = 0;
+    int ties = 0;
+
+//Private functions
+    void update_winner_count(std::string winner);
+};
+#endif
